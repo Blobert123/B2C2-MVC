ab00de63c02e1bf83eb1d9a6221bba051611c524
Robert Zhou <1943669zhou@zuyd.nl>
2023-10-20 13:38:32 +0200

    Add project files.

---
+++ B2C2 2023 Inleveropdracht MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B2C2_2023_Inleveropdracht_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
